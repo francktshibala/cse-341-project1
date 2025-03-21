f1b3aa20469abda32f5224b227b4f8b6a3618d7c
francktshibala <franck1tshibala@gmail.com>
2025-03-21 14:26:18 -0600

    Updating to my project

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  
+
   // Handle preflight requests
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
-  
+
   next();
 });
 
@@ -45,3 +45,5 @@ mongodb.initDb((err) => {
     });
   }
 });
+
+
